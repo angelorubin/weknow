5fe339f7363442cac3d3931653c38f2ab2c741d8
angelorubin <angelorubin@gmail.com>
2021-06-11 10:24:37 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { http } from "./db/config";
+import * as S from "./App.styles";
+import { v4 as uuidv4 } from "uuid";
+
+export const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await http("/products").then((products) => setProducts(products.data));
+    };
+
+    fetchData();
+  }, []);
+
+  const handleRegisterClick = (e) => {
+    const description = e.currentTarget.previousElementSibling.value;
+    console.log(description);
+
+    const addProduct = async () =>
+      await http.post("/products", { id: uuidv4(), description });
+
+    addProduct();
+
+    http("/products").then((resp) => setProducts([...products, resp.data]));
+  };
+
+  const handleDeleteClick = async (e) => {
+    const id = e.currentTarget.id;
+    await http.delete(`/products/${id}`);
+    const getProducts = http("/products").then((products) => products);
+    console.log(getProducts());
+  };
+
+  return (
+    <S.Container>
+      <header>
+        <h1>Cadastrar produto</h1>
+      </header>
+      <main>
+        <label htmlFor="description">descrição</label>
+        <input name="description" type="text" />
+        <button onClick={handleRegisterClick}>cadastrar</button>
+        <hr />
+        <h1>Produtos Cadastrados</h1>
+        {JSON.stringify(products)}
+        {products ? (
+          products.map((product) => {
+            return (
+              <S.ProductGroup key={product.id}>
+                <h4>{product.description}</h4>
+                <S.DeleteButton onClick={handleDeleteClick} id={product.id}>
+                  deletar
+                </S.DeleteButton>
+              </S.ProductGroup>
+            );
+          })
+        ) : (
+          <h1>Nenhum produto cadastrado</h1>
+        )}
+      </main>
+    </S.Container>
+  );
+};
---
+++ src/App.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProductGroup = styled.div`
+  display: flex;
+`;
+
+export const DeleteButton = styled.button`
+  border: none;
+  padding: 5px;
+`;
